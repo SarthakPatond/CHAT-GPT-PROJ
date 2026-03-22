a68d23602532d6493bb3c71213f0fef60aa15db1
SarThug <sarthakpatond99@gmail.com>
2026-03-22 08:26:31 +0530

    first commit

---
+++ src/sockets/socket.server.js
@@ -0,0 +1,12 @@
+const { Server } = require("socket.io");
+
+function initSocketServer(httpServer) {
+
+    const io = new Server(httpServer, {})
+
+    io.on("connection" ,(socket) => {
+        console.log("New Socket connected: " + socket.id);
+    })
+}
+
+module.exports =   initSocketServer;
