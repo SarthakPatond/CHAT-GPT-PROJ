c20da28adaa10462c51ca43d542ceafcaf929c3a
SarThug <sarthakpatond@gmail.com>
2026-04-03 16:03:23 +0530

    fourth commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -27,9 +27,22 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes); 
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public', 'index.html'));
+// })
+
+// Serve frontend build
+app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
+
+// When user opens site
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
+});
+
+// For unknown API routes
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 
 module.exports = app;
